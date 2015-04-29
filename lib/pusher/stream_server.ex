b25f539b3d6b760931db696fe0f905152b37a65d
colinbankier <colinbankier@gmail.com>
2015-04-29 12:40:22 +1000

    Push only page view events for demo.

--- lib/pusher/stream_server.ex
+++ lib/pusher/stream_server.ex
@@ -3,6 +3,15 @@ defmodule Pusher.StreamServer do
 
   def handle event do
     Logger.info "Streamer got event: #{inspect event}"
+    if allow?(event), do: push(event)
+  end
+
+  def allow? event do
+    event["name"] == "page_view"
+  end
+
+
+  def push event do
     Pusher.Endpoint.broadcast!("stream", "event", event)
   end
 end
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,14 +1,17 @@
 import {Socket} from "phoenix"
 
 $(function(){
+  var stream_div = $("#stream");
   console.log('loaded');
   let socket = new Socket("/ws");
   console.log(socket);
   socket.connect();
   socket.join("stream", {}).receive("ok", channel => {
     console.log('joined');
-    channel.on("event", message => {
+    channel.on("event", event => {
       console.log(event);
+      url = event.payload.properties.url;
+      stream_div.prepend('<div>This page <a href="' + url + '">' + url + '</a> was viewed.</div>');
     });
   });
 });
